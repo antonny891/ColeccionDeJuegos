6869ad5b8dd9afadd1961b2084d9ea3c3b714824
antonny <melber.mamanchura@tecsup.edu.pe>
2024-10-02 18:59:48 -0500

    Initial Commit

---
+++ MamanchuraAlccaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MamanchuraAlccaColeccionDeJuegos
+//
+//  Created by Antonny Mmamanchura Alcca on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
